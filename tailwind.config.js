668c4083d285de2fd99c2a37f6420f8329ee7a1d
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-09-20 19:48:17 +0630

    Initialized app, finished installation and configuration of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#fe4a49",
+        blue: "#009fb7",
+        yellow: "#fed766",
+        dark: "#233d4d",
+      },
+    },
+  },
+  plugins: [],
+};
